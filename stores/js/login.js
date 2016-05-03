e312d14f357a470785b5e023d11551e533c4fbc8
aiou <121323561@qq.com>
2016-05-03 16:02:49 +0800

    积分商城

---
+++ stores/js/login.js
@@ -0,0 +1,41 @@
+
+  var curIndex = 0, //当前index
+      imgLen = $(".imgList li").length; //图片总数
+     // 定时器自动变换2.5秒每次
+  var autoChange = setInterval(function(){ 
+    if(curIndex < imgLen-1){ 
+     curIndex ++; 
+    }else{ 
+      curIndex = 0;
+    }
+    //调用变换处理函数
+    changeTo(curIndex); 
+  },2500);
+  //对右下角按钮index进行事件绑定处理等
+  $(".indexList").find("li").each(function(item){ 
+    $(this).hover(function(){ 
+      clearInterval(autoChange);
+      changeTo(item);
+      curIndex = item;
+    },function(){ 
+      autoChangeAgain();
+    });
+  });
+  //清除定时器时候的重置定时器--封装
+  function autoChangeAgain(){ 
+      autoChange = setInterval(function(){ 
+      if(curIndex < imgLen-1){ 
+        curIndex ++;
+      }else{ 
+        curIndex = 0;
+      }
+    //调用变换处理函数
+      changeTo(curIndex); 
+    },2500);
+    }
+  function changeTo(num){ 
+    var goLeft = num * 650;
+    $(".imgList").animate({left: "-" + goLeft + "px"},500);
+    $(".infoList").find("li").removeClass("infoOn").eq(num).addClass("infoOn");
+    $(".indexList").find("li").removeClass("indexOn").eq(num).addClass("indexOn");
+  }
